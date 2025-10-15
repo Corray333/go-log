88b8e1b4b48eb8d1443e3ee41131451677e926fc
Mark Anikin <mark.corray.off@gmail.com>
2025-10-15 16:10:20 +0300

    fix: setup

--- logger.go
+++ logger.go
@@ -1,4 +1,4 @@
-package golog
+package logger
 
 import (
 	"bytes"
@@ -6,9 +6,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"runtime"
 	"strconv"
 	"sync"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
@@ -36,29 +40,31 @@ func colorize(colorCode int, v string) string {
 	return fmt.Sprintf("\033[%sm%s%s", strconv.Itoa(colorCode), v, reset)
 }
 
-type Handler struct {
-	h slog.Handler
-	b *bytes.Buffer
-	m *sync.Mutex
+type handler struct {
+	h           slog.Handler
+	b           *bytes.Buffer
+	m           *sync.Mutex
+	colorize    bool
+	prettyPrint bool
 }
 
-func (h *Handler) Enabled(ctx context.Context, level slog.Level) bool {
+func (h *handler) Enabled(ctx context.Context, level slog.Level) bool {
 	return h.h.Enabled(ctx, level)
 }
 
-func (h *Handler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return &Handler{h: h.h.WithAttrs(attrs), b: h.b, m: h.m}
+func (h *handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &handler{h: h.h.WithAttrs(attrs), b: h.b, m: h.m, colorize: h.colorize, prettyPrint: h.prettyPrint}
 }
 
-func (h *Handler) WithGroup(name string) slog.Handler {
-	return &Handler{h: h.h.WithGroup(name), b: h.b, m: h.m}
+func (h *handler) WithGroup(name string) slog.Handler {
+	return &handler{h: h.h.WithGroup(name), b: h.b, m: h.m, colorize: h.colorize, prettyPrint: h.prettyPrint}
 }
 
 const (
 	timeFormat = "[2006-01-02 15:04:05.000]"
 )
 
-func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
+func (h *handler) Handle(ctx context.Context, r slog.Record) error {
 
 	level := r.Level.String() + ":"
 
@@ -79,28 +85,42 @@ func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
 		attrs["line"] = line
 	}
 
-	bytes, err := json.MarshalIndent(attrs, "", "  ")
+	var attrsBytes []byte
+	if h.prettyPrint {
+		attrsBytes, err = json.MarshalIndent(attrs, "", "  ")
+	} else {
+		attrsBytes, err = json.Marshal(attrs)
+	}
 	if err != nil {
 		return fmt.Errorf("error when marshaling attrs: %w", err)
 	}
 
-	switch r.Level {
-	case slog.LevelDebug:
-		level = colorize(darkGray, level)
-	case slog.LevelInfo:
-		level = colorize(cyan, level)
-	case slog.LevelWarn:
-		level = colorize(lightYellow, level)
-	case slog.LevelError:
-		level = colorize(lightRed, level)
-	}
+	if h.colorize {
+		switch r.Level {
+		case slog.LevelDebug:
+			level = colorize(darkGray, level)
+		case slog.LevelInfo:
+			level = colorize(cyan, level)
+		case slog.LevelWarn:
+			level = colorize(lightYellow, level)
+		case slog.LevelError:
+			level = colorize(lightRed, level)
+		}
 
-	fmt.Println(
-		colorize(lightGray, r.Time.Format(timeFormat)),
-		level,
-		colorize(white, r.Message),
-		colorize(darkGray, string(bytes)),
-	)
+		fmt.Println(
+			colorize(lightGray, r.Time.Format(timeFormat)),
+			level,
+			colorize(white, r.Message),
+			colorize(darkGray, string(attrsBytes)),
+		)
+	} else {
+		fmt.Println(
+			r.Time.Format(timeFormat),
+			level,
+			r.Message,
+			string(attrsBytes),
+		)
+	}
 
 	return nil
 }
@@ -121,24 +141,35 @@ func suppressDefaults(
 	}
 }
 
-func NewHandler(opts *slog.HandlerOptions) *Handler {
+type HandlerOptions struct {
+	*slog.HandlerOptions
+	Colorize    bool
+	PrettyPrint bool
+}
+
+func NewHandler(opts *HandlerOptions) *handler {
 	if opts == nil {
-		opts = &slog.HandlerOptions{}
+		opts = &HandlerOptions{}
+	}
+	if opts.HandlerOptions == nil {
+		opts.HandlerOptions = &slog.HandlerOptions{}
 	}
 	b := &bytes.Buffer{}
 
-	return &Handler{
+	return &handler{
 		b: b,
 		h: slog.NewJSONHandler(b, &slog.HandlerOptions{
 			Level:       opts.Level,
 			AddSource:   opts.AddSource,
 			ReplaceAttr: suppressDefaults(opts.ReplaceAttr),
 		}),
-		m: &sync.Mutex{},
+		m:           &sync.Mutex{},
+		colorize:    opts.Colorize,
+		prettyPrint: opts.PrettyPrint,
 	}
 }
 
-func (h *Handler) computeAttrs(ctx context.Context, r slog.Record) (map[string]any, error) {
+func (h *handler) computeAttrs(ctx context.Context, r slog.Record) (map[string]any, error) {
 	h.m.Lock()
 	defer func() {
 		h.b.Reset()
@@ -156,15 +187,43 @@ func (h *Handler) computeAttrs(ctx context.Context, r slog.Record) (map[string]a
 	return attrs, nil
 }
 
-func SetupCustomLogger() {
-	// Создаем новый обработчик с опциями (можно указать свои).
-	handler := NewHandler(&slog.HandlerOptions{
-		Level: slog.LevelInfo, // Уровень логирования, который вы хотите использовать
-	})
+func SetupLoggerWith(opts *HandlerOptions) {
+	handler := NewHandler(opts)
 
-	// Создаем новый логгер с этим обработчиком.
 	logger := slog.New(handler)
 
-	// Устанавливаем созданный логгер как стандартный.
 	slog.SetDefault(logger)
+}
+
+func new(log *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		log = log.With(
+			slog.String("component", "middleware/logger"),
+		)
+
+		log.Info("logger middleware enabled")
+
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			entry := log.With(
+				slog.String("method", r.Method),
+				slog.String("path", r.URL.Path),
+				slog.String("remote_addr", r.RemoteAddr),
+				slog.String("user_agent", r.UserAgent()),
+				slog.String("request_id", middleware.GetReqID(r.Context())),
+			)
+
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			t1 := time.Now()
+			defer func() {
+				entry.Info("request completed",
+					slog.Int("status", ww.Status()),
+					slog.Int("size", ww.BytesWritten()),
+					slog.Duration("duration", time.Since(t1)),
+				)
+			}()
+			next.ServeHTTP(ww, r)
+		}
+		return http.HandlerFunc(fn)
+	}
 }
